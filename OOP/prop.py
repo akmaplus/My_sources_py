4876cb9e3f85cc5cbb80e2bff91674c222f5695b
akmaplus <akmaplus@rambler.ru>
2012-10-26 11:30:12 +0400

    свойства

---
+++ OOP/prop.py
@@ -0,0 +1,31 @@
+﻿# -*- coding: utf-8 -*-
+
+class Test( object ):
+
+
+    def __init__(self):
+        self.__id = None
+
+    #свойство реализованное лямбдой
+    id = property ( lambda self: self.__id )
+
+    
+    #свойство реализованное методом
+    def getid( self ):
+        return self.__id
+
+    Id = property( getid )
+
+    #аналогично, методом, используя декоратор
+    @property
+    def ID( self ):
+        return self.__id
+
+
+if (__name__=="__main__"):
+
+    obj = Test()
+
+    print obj.id
+    print obj.Id
+    print obj.ID
